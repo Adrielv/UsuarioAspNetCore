ca1970cebf4da403bbe151311c177be136a5432c
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2020-01-11 13:12:50 -0500

    agregando el contexto y la entidades

---
+++ UsuarioAspNetCore/Data/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuarioAspNetCore.Models;
+
+namespace UsuarioAspNetCore.Data
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (Local); Database = TestDb; Trusted_Connection = True;");
+        }
+        //public Context(DbContextOptions<Context> options) : base(options)
+        //{
+        //} 
+    }
+}
---
+++ UsuarioAspNetCore/Models/Usuarios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsuarioAspNetCore.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+
+
+        [Required(ErrorMessage = "Usuario esta incorrecto")]
+        [StringLength(100, ErrorMessage = "Usuario es muy largo")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Contraseñaa esta incorrecto")]
+        [StringLength(100, ErrorMessage = "Contraseña es muy largo")]
+
+        public string Contraseña { get; set; }
+    }
+}
